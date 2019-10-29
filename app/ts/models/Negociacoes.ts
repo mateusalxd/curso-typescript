68f4038db3e0e68ead45327f5d5f543c5626a294
Mateus Alexandre <mateusalxd@gmail.com>
2019-10-29 08:08:20 -0300

    Tipo de retorno do método

--- app/ts/models/Negociacoes.ts
+++ app/ts/models/Negociacoes.ts
@@ -2,11 +2,11 @@ class Negociacoes {
 
     private _negociacoes: Negociacao[] = [];
 
-    adiciona(negociacao: Negociacao) {
+    adiciona(negociacao: Negociacao): void {
         this._negociacoes.push(negociacao);
     }
 
-    paraArray() {
+    paraArray(): Negociacao[] {
         return [].concat(this._negociacoes);
     }
 
